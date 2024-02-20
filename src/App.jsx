ad01a577400505565d1c2cafcc7dce0daf0fda2c
mehdi <mehdimsk95@gmail.com>
2024-02-20 14:54:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button, AppBar } from "@mui/material";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <AppBar>
+        <div className="aaa">
+          <p>PIXIHIVE</p>
+        </div>
+      </AppBar>
+      <br />
+      <br />
+
+      <Button>buttonn </Button>
+    </>
+  );
+}
+
+export default App;
